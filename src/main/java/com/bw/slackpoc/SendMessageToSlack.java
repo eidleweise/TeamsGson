50c9bbee8ac3ceffd19cd48e8a392f0d121892e7
eidleweise <ben.woodward@gmail.com>
2021-10-18 11:28:47 +0100

    Doing some Slack Bolt stuff

--- src/main/java/com/bw/slackpoc/SendMessageToSlack.java
+++ src/main/java/com/bw/slackpoc/SendMessageToSlack.java
@@ -1,5 +1,13 @@
 package com.bw.slackpoc;
 
+
+/**
+ * Some notes -
+ * My Apps - https://api.slack.com/apps
+ * Messaging API - https://api.slack.com/messaging/managing
+ * Tutorial - https://www.woolha.com/tutorials/java-sending-message-to-slack-webhook
+ * Bolt - Maybe what we want to use? https://api.slack.com/tools/bolt
+ */
 public class SendMessageToSlack {
     public static void main(String[] args) {
         SlackMessage slackMessage = SlackMessage.builder()
