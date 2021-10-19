d05e53b12cdbb573e24fc1547fb1e0f92c90f8d2
eidleweise <ben.woodward@gmail.com>
2021-10-19 16:00:10 +0100

    Doing some Slack Bolt stuff

--- src/main/java/com/bw/slackpoc/BoltApp.java
+++ src/main/java/com/bw/slackpoc/BoltApp.java
@@ -2,7 +2,15 @@ package com.bw.slackpoc;
 
 import com.slack.api.bolt.App;
 import com.slack.api.bolt.jetty.SlackAppServer;
+import static com.slack.api.model.block.Blocks.*;
+import static com.slack.api.model.block.composition.BlockCompositions.*;
+import static com.slack.api.model.view.Views.*;
 
+import com.slack.api.methods.response.views.ViewsPublishResponse;
+import com.slack.api.model.event.AppHomeOpenedEvent;
+import com.slack.api.model.view.View;
+
+import static com.slack.api.model.block.element.BlockElements.*;
 
 /**
  * https://api.slack.com/start/building/bolt-java#start
@@ -16,5 +24,28 @@ public class BoltApp {
 
         SlackAppServer server = new SlackAppServer(app);
         server.start(); // http://localhost:3000/slack/events
+
+        app.event(AppHomeOpenedEvent.class, (payload, ctx) -> {
+            View appHomeView = view(view -> view
+                    .type("home")
+                    .blocks(asBlocks(
+                            section(section -> section.text(markdownText(mt -> mt.text("*Welcome to your _App's Home_* :tada:")))),
+                            divider(),
+                            section(section -> section.text(markdownText(mt -> mt.text("This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example on <https://slack.dev/java-slack-sdk/guides/interactive-components|slack.dev/java-slack-sdk>.")))),
+                            actions(actions -> actions
+                                    .elements(asElements(
+                                            button(b -> b.text(plainText(pt -> pt.text("Click me!"))).value("button1").actionId("button_1"))
+                                    ))
+                            )
+                    ))
+            );
+
+            ViewsPublishResponse viewsPublishResponse = ctx.client().viewsPublish(r -> r
+                    .userId(payload.getEvent().getUser())
+                    .view(appHomeView)
+            );
+
+            return ctx.ack();
+        });
     }
 }
